17be126fce2eca9360bbed6c37c7444c309e44df
marxian <rupert@neontribe.co.uk>
2011-01-14 11:38:24 +0000

    More robust handling of missing data

--- widgets/courselabel/courselabel.js
+++ widgets/courselabel/courselabel.js
@@ -20,43 +20,61 @@ iag.display = function(evt) {
 		$('#' + id + ' > span.score').html(formatPercentage(val));
 	}
 	
+	var calculators = {success : function(obj){ return 100 - (obj['First degree']['Full-time']['continuation']['Dormant'] + obj['First degree']['Full-time']['continuation']['Left without award']);},
+					   destination: function(obj){ return 100 - (obj['destinations']['Full-time']['Assumed to be unemployed']['First degree'] + obj['destinations']['Full-time']['Not available for work or study']['First degree']);},
+					   satisfaction: function(obj){ return (obj['First degree']['Full-time']['satisfaction']['Q22']['Actual value']); }
+					  };
+	
+	/**
+	 * Calculate a score
+	 * 
+	 * @param {Object} calc  
+	 * @param {Object} data
+	 * @param {Object} default_result
+	 */			
+	var calculate = function(calc, data, default_result) {
+		try {
+
+			return calculators[calc](data);
+		} catch(err) {
+			console.log(calc);
+			return default_result;
+		}
+	}
+	
 	$('#provider').html(iag.data.name);
 	$('#course').html(iag.course_id);
-
-	var course_stats = iag.data.courses[iag.course_id]['First degree']['Full-time'];
-
-	
+    var course_stats = iag.data.courses[iag.course_id];
+	console.log(course_stats);
 	// Success
-	var success = 100 - (course_stats['continuation']['Dormant'] + course_stats['continuation']['Left without award']);
-	writeScore('success', success);
-	
-	// Destinations
-	var destination_stats = iag.data.courses[iag.course_id]['destinations']['Full-time'];
-
-	var positive = 100 - (destination_stats['Assumed to be unemployed']['First degree'] + destination_stats['Not available for work or study']['First degree'])
-	writeScore('destination', positive);
+	writeScore('success', calculate('success', course_stats, '?'));
 	
 	// Satisfaction
-	var satisfaction = (course_stats['satisfaction']['Q22']['Actual value']);
-	writeScore('satisfaction',satisfaction);
+	writeScore('satisfaction', calculate('satisfaction', course_stats, '?'));
+	
+	// Destinations
+	writeScore('destination', calculate('destination', course_stats, '?'));
 	
 	// Top Jobs
-	
-	// Collapse the keys and values of the jobtypes object for sorting convenience
-	var jobs = [];
-	$.each(course_stats['jobtype'], function(k,v){ v['name'] = k;
+	// Try to find top jobs
+	try {
+		// Collapse the keys and values of the jobtypes object for sorting convenience
+		var jobs = [];
+		$.each(course_stats['First degree']['Full-time']['jobtype'], function(k,v){ v['name'] = k;
 										   jobs.push(v); });
 	
-	// Underscore.js has a nice sortBy...
-	var sorted_jobs = _.sortBy(jobs, function(obj){ return obj['% of students'];  });
+		// Underscore.js has a nice sortBy...
+		var sorted_jobs = _.sortBy(jobs, function(obj){ return obj['% of students'];  });
 	
-	// Grab the highest valued 3 jobtypes and write some list items
-	$.each(sorted_jobs.slice(-3).reverse(), function(){
-		$('#jobs ul').append('<li>'+this.name+' : '+this['% of students']+'%</li>');
-	});
-
+		// Grab the highest valued 3 jobtypes and write some list items
+		$.each(sorted_jobs.slice(-3).reverse(), function(){
+			$('#jobs ul').append('<li>'+this.name+' : '+this['% of students']+'%</li>');
+		});
+	} catch(err) {
+		$('#jobs').hide();
+	}
 	
-	
+		
 	
 }
 
