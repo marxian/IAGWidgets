b8029125c0aacb756704d95c4709d402477dbf00
marxian <rupert@neontribe.co.uk>
2011-01-14 13:46:51 +0000

    Better Greasemonkey

--- greasemonkey/courselabel.user.js
+++ greasemonkey/courselabel.user.js
@@ -7,33 +7,34 @@
 // ==/UserScript==
 
 // Identify the ukprn
-var ukprn = 10003867;//window.location.pathname.substring(window.location.pathname.lastIndexOf('/') + 1, window.location.pathname.length);
-alert('running');
-var course = null;
+var ukprn = 10003867;
+
+// Find the element before which to place our widget
+var insert_before = null;
+try {
+	insert_before = document.getElementsByTagName('table')[0];
+} catch(err) {
+	// Bug out - nowhere to put the widget
+}
+
+var course = 'TEst Course';
 // Identify the course
 try {
-    course = '';
+    course = 'Test Course';
     
 }
 catch (e) {
     alert("Courselabel Monkey can't find a valid course identifier");
 }
 
-if (ukprn) {
-    // Find the element before which to place our widget
-    var insert_before = document.getElementById('content_bottom_left_first');
-    if (insert_before) {
-		script = document.createElement('script');
-		script.type = "text/javascript";
+if (ukprn && course && insert_before) {
+	script = document.createElement('script');
+	script.type = "text/javascript";
         script.src = "http://localhost/widgets/iagwidget.js";
-		var config = '{"ukprn":"'+ ukprn +'",';
-		config += '"widget":"courselabel",';
-		config += '"base_url":"http://localhost",';
-		config += '"course":"' + course +'"}';
+	var config = '{"ukprn":"'+ ukprn +'",';
+	config += '"widget":"courselabel",';
+	config += '"base_url":"http://localhost",';
+	config += '"course":"' + course +'"}';
         script.innerHTML = config;
         insert_before.parentNode.insertBefore(script, insert_before);
-    } else {
-        alert("Courselabel Monkey failed to find a suitable place to put the widget");
-    }
 }
-
